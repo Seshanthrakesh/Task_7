3b88583d55de8ba4bae75a32d62bc0f8739b68b4
seshnathrakesh <seshanthrakesh1901@gmail.com>
2023-09-07 16:19:21 +0530

    question1 is commited

---
+++ src/basic/Question1.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c=30,d=40;
+		int e=a+b;
+		int f=c+d;
+		if(e<f)
+		{
+			System.out.println("The sum of a and b is greater than sum of c and d");
+			}
+		else 
+			System.out.println();
+			
+		
+	}
+
+}
