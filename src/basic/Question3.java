b5b620e3740de9f4dbc3a61d66b76f48f8b1a601
seshanth <seshanthrakesh1901@gmail.com>
2023-09-07 17:43:12 +0530

    qn3 is updated

---
+++ src/basic/Question3.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 char ch ;
+			for ( ch = 'A'; ch <= 'Z' ; ch++)
+			{
+				System.out.println(ch + " ");
+			}
+	}
+
+}
