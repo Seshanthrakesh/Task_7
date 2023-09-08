1750a33756018da2fd9b99f712b6709f3fe95feb
seshanth <seshanthrakesh1901@gmail.com>
2023-09-08 16:00:27 +0530

    Question 6 is commited

---
+++ src/basic/Question6.java
@@ -0,0 +1,20 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int num;
+			Scanner sc=new Scanner(System.in);
+			//Scanner sc=new Scanner(System.in);
+			System.out.print("Enter the number: ");
+			num = sc.nextInt();
+			
+			
+			
+
+	}
+
+}
